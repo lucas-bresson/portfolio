5d81bddb10e700e304ab38edd9c358fa72a54fa9
lucas-bresson <contact.bresson@gmail.com>
2024-10-26 10:13:15 +0200

    fix stroke-width to strokeWidth

--- src/app/bookmark/components/FAQSection.tsx
+++ src/app/bookmark/components/FAQSection.tsx
@@ -17,7 +17,7 @@ const Tab = ({
           <path
             fill="none"
             stroke="currentColor"
-            stroke-width="3"
+            strokeWidth="3"
             d="M1 1l8 8 8-8"
           />
         </svg>
